a6f05d8faa46fd9aa8f72710b6ffdd2bf1b7daf6
Dileep77 <1999dileep@gmail.com>
2021-12-04 14:57:07 +0530

    commit changes 4

--- carztestpro/settings.py
+++ carztestpro/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carztestpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carstest_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Dileep77',
+        'HOST': 'localhost'
     }
 }
 
